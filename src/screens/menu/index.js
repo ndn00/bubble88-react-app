91a15553ca51a8f4f38e58d4ebf30ade227fea33
Duy Nguyen <ndnguyen@sfu.ca>
2020-03-21 02:11:21 -0700

    Refactored MenuScreen

--- src/screens/menu/index.js
+++ src/screens/menu/index.js
@@ -10,6 +10,10 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+
+import ScrollableTabView, { ScrollableTabBar, } from 'react-native-scrollable-tab-view';
+
+
 import TopLogo from '../../components/toplogo/index';
 import SmallButton from '../../components/buttons/smallbutton/index'
 import MenuItem from '../../components/menu_item/index'
@@ -17,6 +21,15 @@ import {addItem} from '../../redux/actions';
 
 import { FONT_TITLE, PRIMARY_COLOR, FONT_TEXT, MARGIN } from '../../styles';
 import bubble88 from '../../mock_db/menu.json';
+
+
+
+const category2index = {
+  "Noodles": 0,
+  "Rice": 1
+}
+
+
 
 const categories = [
   { key: 0, title: 'All'},
@@ -35,8 +48,14 @@ const categories = [
 class MenuScreen extends Component {
 
   state = {
-    currentIndex: 0
+    currentIndex: 0,
   }
+
+  // componentDidMount(){
+  //   if(category2index[this.props.route.params.category]!==this.currentIndex){
+  //     this.setState({currentIndex: category2index[this.props.route.params.category]})
+  //   }
+  // }
 
   handleChange = item => {
     this.setState({currentIndex: item.key})
@@ -46,45 +65,82 @@ class MenuScreen extends Component {
     this.props.addItem(item)
   }
 
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       <TopLogo/>
+  //       <View style={{height: 50}}>
+  //         <FlatList
+  //           horizontal={true}
+  //           showsHorizontalScrollIndicator={false}
+  //           data={categories}
+  //           renderItem={({item}) => (
+  //             <TouchableOpacity style={{
+  //                 borderRightWidth: 2,
+  //                 width: 120,
+  //                 height: 40,
+  //                 alignItems: 'center',
+  //                 justifyContent: 'center'
+  //               }}
+  //               underlayColor={'orange'}
+  //               onPress={() => this.handleChange(item)}
+  //             >
+  //               <Text style={FONT_TEXT}>{item.title}</Text>
+  //             </TouchableOpacity>
+  //           )}
+  //           keyExtractor = {(item) => item.key}
+  //         />
+  //       </View>
+  //       <ScrollView
+  //         tabLabel={'Noodles'}
+  //         style={styles.container}
+  //       >
+  //         {bubble88['menu'].map((item) =>
+  //           <MenuItem item={{
+  //             image: 'http://www.bubble88.com/wp-content/uploads/2015/03/noodle.jpg',
+  //             name: item.english_name + '\n' + item.alt_name,
+  //             price: item.price,
+  //             handleSubmit: ()=>this.handleAdd(item),
+  //           }}/>
+  //         )}
+  //       </ScrollView>
+  //     </View>
+  //   );
+  // }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <TopLogo/>
-        <View style={{height: 50}}>
+      <View style={styles.container}>
+        <TopLogo/>  
+        <ScrollableTabView
+          key={this.props.route.params.key}
+          style={{ marginTop: 0 }}
+          initialPage={category2index[this.props.route.params.category]}
+          renderTabBar={() => <ScrollableTabBar 
+            activeTextColor='orange' 
+            underlineStyle={{backgroundColor: 'orange'}}
+            textStyle={FONT_TITLE}
+            tabStyle={{height: FONT_TITLE.fontSize*2*1.6}}
+            style={{height: FONT_TITLE.fontSize*2*1.6}}
+            />}
+        >
+        {categories.map(category => (
           <FlatList
-            horizontal={true}
-            data={categories}
-            renderItem={({item}) => (
-              <TouchableOpacity style={{
-                  borderRightWidth: 2,
-                  width: 120,
-                  height: 40,
-                  alignItems: 'center',
-                  justifyContent: 'center'
-                }}
-                underlayColor={'orange'}
-                onPress={() => this.handleChange(item)}
-              >
-                <Text style={FONT_TEXT}>{item.title}</Text>
-              </TouchableOpacity>
-            )}
-            keyExtractor = {(item) => item.key}
-          />
-        </View>
-          <View
-            tabLabel={'Noodles'}
-            style={styles.container}
-          >
-            {bubble88['menu'].map((item) =>
-              <MenuItem item={{
-                image: 'http://www.bubble88.com/wp-content/uploads/2015/03/noodle.jpg',
-                name: item.english_name + '\n' + item.alt_name,
-                price: item.price,
-                handleSubmit: ()=>this.handleAdd(item),
-              }}/>
-            )}
-          </View>
-      </ScrollView>
+          tabLabel={category.title}
+          data={bubble88['menu']}
+          renderItem={({item}) =>
+            <MenuItem item={{
+              image: 'http://www.bubble88.com/wp-content/uploads/2015/03/noodle.jpg',
+              name: item.english_name + '\n' + item.alt_name,
+              price: item.price,
+              handleSubmit: ()=>this.handleAdd(item),
+            }}/>}
+          keyExtractor = {(item, index) => index.toString() }
+        />
+        ))}
+          
+        </ScrollableTabView>
+      </View>
     );
   }
 }
